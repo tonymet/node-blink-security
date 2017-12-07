e7f92d252ff0c2e869b87f95064f62b3952b1517
Anthony Metzidis <anthony.metzidis@gmail.com>
2017-12-07 15:19:14 -0800

    initial test suite

--- test/index.js
+++ test/index.js
@@ -1,19 +1,45 @@
-require('dotenv').config()
+/* eslint-env node, mocha */
+require('dotenv').config();
 var chai = require('chai');
 chai.use(require('chai-http'));
 chai.use(require('chai-as-promised'));
-var expect = chai.expect
-chai.should()
-const Replay  = require('replay');
-var Blink = require('../index.js')
+chai.should();
+require('replay');
+var Blink = require('../index.js');
 
-describe('description', () => {
-  var blink;
+describe('Auth Token Login', () => {
+  let blink;
   before(() => {
     blink = new Blink('', '', {_token: process.env.BLINK_AUTH_TOKEN, _region_id: process.env.BLINK_REGION_ID, _network_id:process.env.BLINK_NETWORK_ID});
-    blink.setupSystem()
-  })
+    return blink.setupSystem().should.be.fulfilled;
+  });
+  after(() => {
+    blink = null;
+  });
   it('should get clients', () => {
-    return blink.getClients().should.eventually.have.deep.property('clients')
+    return blink.getClients().should.eventually.have.deep.property('clients');
+  });
+  it('should get cameras', () => {
+    return blink.getIDs().should.eventually.have.deep.property('_cameras');
+  });
+});
+
+describe('Password Login', () => {
+  let blink;
+  before(() => {
+    blink = new Blink('username', 'password');
+    return blink.setupSystem().should.be.fulfilled;
+  });
+  after(() => {
+    blink = null;
+  });
+  it('should get clients', () => {
+    return blink.getClients().should.eventually.have.deep.property('clients');
+  });
+  it('should get cameras', () => {
+    return blink.getIDs().should.eventually.have.deep.property('_cameras');
+  });
+  it('should setupSystem properly', () => {
+    return blink.should.have.deep.property('_cameras');
   });
 });
